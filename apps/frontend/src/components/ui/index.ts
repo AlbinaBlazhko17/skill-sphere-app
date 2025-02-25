edb6e4e268b4e944ff65973f8d5cd1f50ced6e85
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-25 13:48:57 +0200

    feat: add public api for ui folder

---
+++ apps/frontend/src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from './button';
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
+export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, useFormField } from './form';
+export { Label } from './label';
+export { Input } from './input';
