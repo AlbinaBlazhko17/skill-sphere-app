a8a94ef88e36a206139efde5054681c463cf16aa
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-25 14:28:09 +0200

    chore: remove files

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './pages/App';
+import { RouterProvider } from 'react-router';
+import { router } from '@/libs/router/router.config';
 
 import '@/libs/styles/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
