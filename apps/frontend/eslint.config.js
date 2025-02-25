568ed0f384253fc96061ff9d9721d90e7f649640
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-02-25 16:50:55 +0200

    feat: add RTL and Jest, configure eslint

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -2,7 +2,9 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import reactPlugin from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
+import testingLibrary from 'eslint-plugin-testing-library';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -14,11 +16,15 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'testing-library': testingLibrary,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...reactPlugin.configs['recommended'].rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
       'react/jsx-no-target-blank': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/jsx-indent': [2, 'tab'],
