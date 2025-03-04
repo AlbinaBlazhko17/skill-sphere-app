0a0fcac5389420b97c8e2c9c8c93841ed6933435
Albina Blazhko <46962291+AlbinaBlazhko17@users.noreply.github.com>
2025-03-04 14:54:04 +0200

    fix: toast styles

--- apps/frontend/src/components/ui/sonner.tsx
+++ apps/frontend/src/components/ui/sonner.tsx
@@ -5,19 +5,20 @@ const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme();
 
   return (
-	<Sonner
-		theme={theme as ToasterProps['theme']}
-		className="toaster group"
-		toastOptions={{
+    <Sonner
+      theme={theme as ToasterProps['theme']}
+      className="toaster group"
+      toastOptions={{
         classNames: {
           toast:
-            'group toast group-[.toaster]:bg-violet-50 group-[.toaster]:text-violet-950 group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+            'group toast group-[.toaster]:!bg-violet-50 group-[.toaster]:!text-violet-950 group-[.toaster]:!border-violet-800 group-[.toaster]:!shadow-md',
           description: 'group-[.toast]:text-muted-foreground',
           actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium',
-          cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground font-medium',
+          cancelButton: 'group-[.toast]:!bg-violet-50 group-[.toast]:!text-violet-950 font-medium',
+          icon: 'group-data-[type=error]:text-red-500 group-data-[type=success]:text-green-500',
         },
       }}
-		{...props}
+      {...props}
     />
   );
 };
